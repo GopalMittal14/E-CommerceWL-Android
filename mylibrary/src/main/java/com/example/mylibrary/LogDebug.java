edbc0e6ff8fbcc01b6aacd89e591b120bddef2ab
Gopal_Mittal_PS <gopal.mittal@publicissapient.com>
2022-06-02 16:33:42 +0530

    added my library.

---
+++ mylibrary/src/main/java/com/example/mylibrary/LogDebug.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+public class LogDebug {
+    private static final String TAG="Message";
+
+    public static void d(String message)
+    {
+        Log.d(TAG,message);
+    }
+}
